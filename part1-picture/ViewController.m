e22918d9785d514d3400c533b786e960402cc379
小米条 <zhoutingtt@163.com>
2015-06-13 00:56:30 -0700

    第二版，终于有点样子啦！

--- part1-picture/ViewController.m
+++ part1-picture/ViewController.m
@@ -15,12 +15,43 @@
 @property (weak, nonatomic) IBOutlet UIButton *rightBtn;
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
+@property (nonatomic, assign)int index;
+@property (nonatomic,strong)NSArray *imageDicts;
+
 @end
 
 @implementation ViewController
+
+- (NSArray *)imageDicts{
+
+    if (!_imageDicts) {
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"imageDate.plist" ofType:nil];
+        _imageDicts = [NSArray arrayWithContentsOfFile:path];
+    
+    }
+        return _imageDicts;
+}
+
 - (IBAction)leftBtnOnclick:(UIButton *)sender {
+    self.index++;
+    [self BtnClickChange];
+    
 }
 - (IBAction)rightBtnOnclick:(id)sender {
+    self.index++;
+    [self BtnClickChange];
+    
+}
+
+-(void)BtnClickChange{
+    self.topLabel.text = [NSString stringWithFormat:@"%d/%d", (self.index+1),self.imageDicts.count];
+    
+    self.detailLabel.text = self.imageDicts[self.index][@"description"];
+    self.imageView.image = [UIImage imageNamed:self.imageDicts[self.index][@"name"]];
+    
+    self.leftBtn.enabled =(self.index != 0);
+    self.rightBtn.enabled =(self.index!= 4);
+
 }
 
 - (void)viewDidLoad {
